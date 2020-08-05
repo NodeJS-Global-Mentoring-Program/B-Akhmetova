3469511cb34f99536554726d84ca1f865d5ce365
bakhytgulAkhmetova1 <omron2020@bk.ru>
2020-08-05 09:31:22 +0400

    reverse string changes

--- module_1/task1.js
+++ module_1/task1.js
@@ -2,14 +2,7 @@ const readline = require('readline');
 
 const rl = readline.createInterface({ input: process.stdin });
 
-const reverseString = (input) => {
-    let output = '';
-    for (let i = input.length - 1; i >= 0; i--) {
-        output += input[i];
-    }
-    return output;
-}
-
 rl.on('line', (input) => {
-    console.log(reverseString(input));
+    const output = input.split('').reverse().join('');
+    console.log(output);
 });
--- module_1/task3.js
+++ module_1/task3.js
@@ -7,16 +7,9 @@ import { pipeline } from 'stream';
 
 const rl = readline.createInterface({ input: process.stdin });
 
-const reverseString = (input) => {
-    let output = '';
-    for (let i = input.length - 1; i >= 0; i--) {
-        output += input[i];
-    }
-    return output;
-}
-
 rl.on('line', (input) => {
-    console.log(reverseString(input));
+    const output = input.split('').reverse().join('');
+    console.log(output);
 });
 
 // task 2
