9e268972498ef68af76b8e1db09d00b6b886c504
bakhytgulAkhmetova1 <omron2020@bk.ru>
2020-09-08 17:02:33 +0400

    change sequilize structure

--- module_4/src/data-access/initUserGroups.sql
+++
@@ -1,5 +0,0 @@
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-CREATE TABLE IF NOT EXISTS "UserGroup" (
-    "userId" uuid DEFAULT uuid_generate_v4 (),
-    "groupId" uuid DEFAULT uuid_generate_v4 ()
-);
